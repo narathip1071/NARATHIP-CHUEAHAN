b249e2a1e2dd462bb7d0cffff96253f926fb4b1b
narathip1071 <78547972+narathip1071@users.noreply.github.com>
2021-03-12 11:26:53 +0700

    Add files via upload

---
+++ homework3.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace homework3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string mode;
+            double x, y;
+
+            Console.Write("mode:");
+            mode = Console.ReadLine();
+            Console.Write("x : ");
+            x = double.Parse(Console.ReadLine());
+            Console.Write("y : ");
+            y = double.Parse(Console.ReadLine());
+
+            if (mode == "time" || mode == "price")
+            {
+                if (x >= 0)
+                {
+                    if (mode == "time")
+                    {
+                        y = Math.Pow((x - 1), 2);
+                        Console.WriteLine("{0}, {1}", x, y);
+                    }
+                    else if (mode == "price")
+                    {
+                        x = Math.Sqrt(y) + 1;
+                        Console.WriteLine("{0}, {1}", x, y);
+                    }
+                }
+                else if (x < 0)
+                {
+                    Console.WriteLine("Invalid mouse position");
+                }
+            }
+            else Console.WriteLine("Invalid mode");
+
+
+        }
+    }
+    
+}
