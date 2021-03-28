3c0bd20a3438be28e29e86afa49d1427c39c77b7
narathip1071 <78547972+narathip1071@users.noreply.github.com>
2021-03-28 21:48:42 +0700

    Add files via upload

---
+++ สอบข้อ3.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Practice_Exam_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int sum,a, b, c;
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+            c = int.Parse(Console.ReadLine());
+            sum = a + b + c;
+            while (sum >= 0)
+            {
+                Console.Write("tank A: ");
+                int tankA = int.Parse(Console.ReadLine());
+                Console.Write("box A : ");
+                int boxA = int.Parse(Console.ReadLine());
+                sum = sum - boxA;
+                if(sum==0)
+                {
+                    Console.WriteLine("The winner is : B");
+                    break;
+                }
+
+                Console.Write("tank B: ");
+                int tankB = int.Parse(Console.ReadLine());
+                Console.Write("box B : ");
+                int boxB = int.Parse(Console.ReadLine());
+                sum = sum - boxB;
+                if(sum==0)
+                {
+                    Console.WriteLine("The winner is : A");
+                    break;
+                }
+            }
+        }
+    }
+    
+}
