ab45698d02f02e10b40435becb9016cf544cf8d0
narathip1071 <78547972+narathip1071@users.noreply.github.com>
2021-03-28 21:44:32 +0700

    Add files via upload

---
+++ สอบข้อ 2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Practice_Exam_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, x, y;
+             a = int.Parse(Console.ReadLine());
+             b= int.Parse(Console.ReadLine());
+            x = a;
+            y = b;
+
+            do
+            {
+                if (x < y)
+                {
+                    x = x + a;
+
+                }
+                else
+                {
+                    y = y + b;
+                }
+            } while (x != y);
+
+            Console.Write("{0}", x);
+        }
+    }
+}
