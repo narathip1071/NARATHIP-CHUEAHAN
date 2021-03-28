d6b07ee45f14a987eaba0d555ae75419e68e1ad2
narathip1071 <78547972+narathip1071@users.noreply.github.com>
2021-03-28 22:57:10 +0700

    Add files via upload

---
+++ ข้อ2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ข้อ2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, x, y;
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+            x = a;
+            y = b;
+
+            do
+            {
+                if (x < y)
+                {
+                    x = x + a;
+
+                }
+                if( x > y)
+                {
+                    y = y + b;
+                }
+            } while (x != y);
+
+            Console.Write("{0}", x);
+        }
+    }
+}
