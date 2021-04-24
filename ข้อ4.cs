af8357a545a3e82bbfd71c4186417c4a2ff7514e
narathip1071 <78547972+narathip1071@users.noreply.github.com>
2021-04-24 15:55:56 +0700

    Add files via upload

---
+++ ข้อ4.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace สอบ4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double dx, dy, step, x, y;
+            float x1 = float.Parse(Console.ReadLine());
+            float y1 = float.Parse(Console.ReadLine());
+            float x2 = float.Parse(Console.ReadLine());
+            float y2 = float.Parse(Console.ReadLine());
+
+            dx = x2 - x1;
+            dy = y2 - y1;
+            {
+                if (Math.Abs(dx) >= Math.Abs(dy))
+                {
+                    step = Math.Abs(dx);
+                    dx /= step;
+                }
+                else
+                {
+                    step = Math.Abs(dy);
+                    dy /= step;
+                }
+
+                x = x1;
+                y = y1;
+                int i = 1;
+
+                for (i = 1; i <= step; i++)
+                {
+                    x += dx;
+                    y += dy;
+                }
+                Console.WriteLine(i);
+            }
+
+        }
+    }
+}
