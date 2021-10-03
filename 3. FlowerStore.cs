87ce0571490c5b46faeff08745b490d70c3767bb
narathip1071 <78547972+narathip1071@users.noreply.github.com>
2021-10-03 09:43:42 +0700

    Add files via upload

---
+++ 3. FlowerStore.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Flowershop_code
+{
+    class FlowerStore//แยก class เพื่อให้ง่ายต่อการอ่านและแก้ไข
+    {
+        public List<string> flowerList = new List<string>();
+        List<string> cart = new List<string>();
+
+        public FlowerStore()
+        {
+            flowerList.Add("Rose");
+            flowerList.Add("Lotus");
+        }
+
+        public void addToCart(string name)
+        {
+            cart.Add(name);
+        }
+
+        public void showCart()
+        {
+
+            if (cart.Count == 0)
+            {
+                Console.WriteLine("Cart is empty");
+            }
+            else
+            {
+                Console.WriteLine("My Cart :");
+                foreach (string i in cart)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
