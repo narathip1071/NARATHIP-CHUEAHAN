fda4cef92147d456dd38011e3503dc427dfa149c
narathip1071 <78547972+narathip1071@users.noreply.github.com>
2021-12-16 12:36:46 +0700

    Add files via upload

---
+++ Final9.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input skill name:");
+            String Nameskill = Console.ReadLine();
+
+        }
+    }
+}
